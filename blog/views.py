24b18d44c1eaa93674692e1b9d060197f2f002d5
Gioele <gioele.gg@gmail.com>
2021-09-12 19:54:47 +0200

    one shot character images

--- blog/views.py
+++ blog/views.py
@@ -15,3 +15,7 @@ class LastPost(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
+class Characters(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/characters.html'

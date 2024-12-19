cc2088de4717e2beb22e7b40ecf2ca9c0b75a174
giocasin <gioele.gg@gmail.com>
2024-12-19 16:10:33 +0100

    added frame script

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
 function GetCurrentYear() {
     document.getElementById("footer-text-date").innerHTML = new Date().getFullYear();
 }
+
+function PageIsInFrame() {
+    if (document.getElementsByTagNameNS("frame") !== undefined) {
+        
+        var sectionBlocks = document.getElementsByClassName("section-block");
+
+        for (let sectionBlock in sectionBlocks) {
+            sectionBlock.classList.remove("section-block");
+            sectionBlock.classList.add("section-block-framed");
+        }
+    }
+}
